566dc4cbc40061c00c8d9db8f3be2cad0c5430fa
Oleksandr Bazhyn <olexandrbazhyn@gmail.com>
2023-03-13 22:28:14 +0200

    the end

--- main.py
+++ main.py
@@ -2,14 +2,12 @@ import solitaire
 
 game = solitaire.Game()
 
-game.show_table()
+# for i in range(1, 7+1):
+#     game.GC_to_RC(1, i)
 
-for i in range(1, 7+1):
-    game.GC_to_RC(1, i)
-
-game.show_table()
+# game.show_table()
 
 
-for j in range (1, 4+1):
-    game.deck_to_GC(j)
-    game.show_table()
+# for j in range (1, 4+1):
+#     game.deck_to_GC(j)
+#     game.show_table()
